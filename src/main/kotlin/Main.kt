df2fef5d68758d22c067a6c0397f9d2f50c36c62
dsouzajm <danielsouza.jm@gmail.com>
2023-09-05 17:03:44 -0300

    Uso da estrutura de repeticao.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,26 +5,32 @@ import java.util.*
 fun main() {
 
     var scanner = Scanner(System.`in`)
-    println("Digite o codigo do jogo: ")
-    var codigoDigitado = scanner.nextLine()
-    var serviceApiJogo = ApiJogo()
-    val meuInfoJogo = serviceApiJogo.buscaJogo(codigoDigitado)
-    var meuJogo:Jogo? = null
 
-    var resultado = runCatching {
-        meuJogo = Jogo(meuInfoJogo.info.title,
-                       meuInfoJogo.info.thumb)
-    }
+    do {
+        println("Digite o codigo do jogo: ")
+        var codigoDigitado = scanner.nextLine()
+        var serviceApiJogo = ApiJogo()
+        val meuInfoJogo = serviceApiJogo.buscaJogo(codigoDigitado)
+        var meuJogo:Jogo? = null
 
-    resultado.onFailure {
-        println("Erro, tente outro id.")
-    }
+        var resultado = runCatching {
+            meuJogo = Jogo(meuInfoJogo.info.title,
+                           meuInfoJogo.info.thumb)
+        }
 
-    resultado.onSuccess {
-        println("Digite a descricao:")
-        val descricaoDigitada = scanner.nextLine()
-        meuJogo?.descricao = descricaoDigitada
-        println(meuJogo)
-        println("Chamada da API realizada com sucesso.")
-    }
+        resultado.onFailure {
+            println("Erro, tente outro id.")
+        }
+
+        resultado.onSuccess {
+            println("Digite a descricao para preenchimento do filme retornado:")
+            val descricaoDigitada = scanner.nextLine()
+            meuJogo?.descricao = descricaoDigitada
+            println(meuJogo)
+        }
+        println("Deseja prosseguir com a busca? Digite S/N.")
+        val respostaContinuacao = scanner.nextLine()
+    } while (respostaContinuacao.equals("S", true))
+
+    println("Chamada da API realizada com sucesso.")
 }
